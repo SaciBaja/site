301785f188d23e2aa99ad4fd46a8897aa5926c1b
leticiariib <d2024010070@unifei.edu.br>
2026-07-24 12:39:29 -0300

    att pinça

--- script.js
+++ script.js
@@ -146,3 +146,18 @@ var swiper = new Swiper(".mySwiper", { // ou a sua classe do swiper
         },
     },
 });
+
+document.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+    const now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
